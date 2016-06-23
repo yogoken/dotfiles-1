f2929466c5c734cdc180d111f28e2613d8bd2521
sa-inu <uovo.aureo.000@gmail.com>
2016-06-23 11:32:27 +0900

    [add] gitconfig and gitignore to dotfiles

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .gvimrc)
+DOT_FILES=(.bashrc .bash_profile .zshrc .vimrc .gvimrc .gitconfig .gitignore)
 
 for file in ${DOT_FILES[@]}
 do
